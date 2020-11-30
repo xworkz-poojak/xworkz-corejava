cb52e8175b9e009ca52cbb4a669ab5ff13aa5050
xworkz-poojak <74228055+xworkz-poojak@users.noreply.github.com>
2020-11-30 11:59:27 +0530

    Add files via upload

---
+++ EarPhoneUtil.java
@@ -0,0 +1,15 @@
+class EarPhoneUtil
+{
+public static void main(String aa[])
+{
+ EarPhone erph=new EarPhone();
+ //erph.name="sumsung";
+ System.out.println(erph.name);
+ //erph.colour="black";
+ System.out.println(erph.colour);
+ //erph.cost="250";
+ System.out.println(erph.cost);
+ erph.usedTo();
+ erph.usedFor();
+}
+}
