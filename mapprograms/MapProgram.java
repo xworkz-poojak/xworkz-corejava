f290ccc04f7b70b52afe63253d50e1db48300f75
xworkz-poojak <74228055+xworkz-poojak@users.noreply.github.com>
2021-02-11 08:52:33 +0530

    Add files via upload

---
+++ mapprograms/MapProgram.java
@@ -0,0 +1,44 @@
+import java.util.Optional;
+
+public class MapProgram {
+
+	public static void main(String[] args) {
+
+		String email = null;
+
+		System.out.println(email);
+
+		if (email != null) {
+			System.out.println(email.toUpperCase());
+		}
+
+		Optional<String> optional = Optional.of("sahana@xworkz.com");
+
+		System.out.println(optional.isPresent());
+
+		if (optional.isPresent()) {
+			System.out.println(optional.get());
+		}
+
+		Optional<Integer> yearOptional = Optional.empty();
+
+		System.out.println(yearOptional.isPresent());
+
+		yearOptional.ifPresent((v) -> System.out.println(v));
+
+		Optional<String> country = Optional.ofNullable("India");
+
+		country.ifPresent(e -> {
+			String value = country.map(String::toUpperCase).get();
+			System.out.println(value);
+
+		});
+
+		String def = country.orElse("Srilanka");
+		System.out.println(def);
+
+	}
+
+}
+
+
